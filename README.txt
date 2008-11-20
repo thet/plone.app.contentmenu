9dd9d1bcfd6bf2bf35f9691698dc044cec9397e1
Hanno Schlichting <hanno@hannosch.eu>
2008-11-20 13:48:51 +0000

    Made the trunk version 2.0 and cleaned up boilerplate
    svn path=/plone.app.contentmenu/trunk/; revision=23387

--- README.txt
+++ README.txt
@@ -3,4 +3,3 @@ Introduction
 
 plone.app.contentmenu contains the logic that powers Plone's content menu
 (the green one with the drop-down menus).
-
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1.7'
+version = '2.0'
 
 setup(name='plone.app.contentmenu',
       version=version,
       description="Plone's content menu implementation",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
       keywords='plone contentmenu menu',
-      author='Martin Aspeli',
-      author_email='optilude@gmx.net',
-      url='http://svn.plone.org/svn/plone/plone.app.contentmenu',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.contentmenu',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages = ['plone', 'plone.app'],
@@ -24,7 +24,4 @@ setup(name='plone.app.contentmenu',
         'plone.memoize',
         'plone.app.content',
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
       )
